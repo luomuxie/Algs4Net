dc8dce0345c4e917f9b0594223ff3fccca667a95
luomuxie <835657341@qq.com>
2020-10-29 17:54:08 +0800

    update 一些调试更改

--- Algs4NetTests/BinarySearchTests.cs
+++ Algs4NetTests/BinarySearchTests.cs
@@ -16,12 +16,12 @@ namespace Algs4NetUnitTests
     {
       int[] a = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 22 };
 
-      Assert.AreEqual(-1, BinarySearch.IndexOf(a, 1));
-      Assert.AreEqual(-1, BinarySearch.IndexOf(a, 20));
-      Assert.AreEqual(-1, BinarySearch.IndexOf(a, 25));
-      Assert.AreEqual(1, BinarySearch.IndexOf(a, 4));
-      Assert.AreEqual(5, BinarySearch.IndexOf(a, 12));
-      Assert.AreEqual(9, BinarySearch.IndexOf(a, 22));
+        Assert.AreEqual(-1, BinarySearch.IndexOf(a, 1));
+        Assert.AreEqual(-1, BinarySearch.IndexOf(a, 20));
+        Assert.AreEqual(-1, BinarySearch.IndexOf(a, 25));
+        Assert.AreEqual(1, BinarySearch.IndexOf(a, 4));
+        Assert.AreEqual(5, BinarySearch.IndexOf(a, 12));
+        Assert.AreEqual(9, BinarySearch.IndexOf(a, 22));
     }
   }
 }
